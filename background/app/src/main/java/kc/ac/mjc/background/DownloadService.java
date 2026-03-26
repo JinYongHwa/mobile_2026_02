c76ff4ee3d46c8a1fc782d2a749a2c12e999c5fc
jin yonghwa <yhjin@hhsoft.co.kr>
2026-03-26 16:47:04 +0900

    03/26

---
+++ background/app/src/main/java/kc/ac/mjc/background/DownloadService.java
@@ -0,0 +1,76 @@
+package kc.ac.mjc.background;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.Service;
+import android.content.Intent;
+import android.os.Build;
+import android.os.IBinder;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+import androidx.core.app.NotificationCompat;
+
+public class DownloadService extends Service {
+
+    int progress=0;
+
+    NotificationCompat.Builder builder;
+    final int NOTIFICATION_ID=1000;
+    final String CHANNEL_NAME="download";
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        //백그라운드에서 실행되는 코드
+        NotificationManager nm=getSystemService(NotificationManager.class);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel=new NotificationChannel(CHANNEL_NAME,CHANNEL_NAME,NotificationManager.IMPORTANCE_LOW);
+            nm.createNotificationChannel(channel);
+        }
+        builder=new NotificationCompat.Builder(this,CHANNEL_NAME)
+                .setContentTitle("다운로드 중")
+                .setProgress(100,progress,false)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setOngoing(true);
+        nm.notify(NOTIFICATION_ID,builder.build());     //알림을 띄운다
+
+
+        Thread t=new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for(int i=0;i<=100;i+=10){
+                    progress=i;
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    Log.d("DownloadService","progress:"+progress);
+
+                    //알림차에 바뀐 progress 적용하기
+                    builder.setProgress(100,progress,false);
+                    nm.notify(NOTIFICATION_ID,builder.build());
+
+                    if(progress>=90){   //progress 90이되면
+                        nm.cancel(NOTIFICATION_ID);
+                    }
+                    
+                    Intent intent=new Intent();
+                    intent.setAction("download_progress");  //이 채널명으로 방송
+                    intent.putExtra("progress",progress);
+                    intent.setPackage(getPackageName());
+                    sendBroadcast(intent);      //progress 값 앱내에 송출
+                }
+            }
+        });
+        t.start();
+
+        return super.onStartCommand(intent, flags, startId);
+    }
+}
---
+++ background/app/src/main/java/kc/ac/mjc/background/MainActivity.java
@@ -0,0 +1,77 @@
+package kc.ac.mjc.background;
+
+import static android.content.pm.PackageManager.PERMISSION_GRANTED;
+
+import android.Manifest;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    ProgressBar downloadPb;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        Button downloadBtn=findViewById(R.id.download_btn);
+        downloadPb=findViewById(R.id.download_pb);
+        
+        
+        //다운로드 버튼이 클릭 됐을때
+        downloadBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,DownloadService.class);
+                startService(intent);
+                //finish();   //액티비티 종료
+            }
+        });
+
+        //알림 권한이 있는지 확인하기
+        int status=checkSelfPermission(Manifest.permission.POST_NOTIFICATIONS);
+        if(status!= PERMISSION_GRANTED){    //사용자가 권한 허용을 하지 않은경우
+
+            //사용자에게 권한 요청하기
+            requestPermissions(new String[]{
+                    Manifest.permission.POST_NOTIFICATIONS
+            },1000);
+        }
+
+        //방송을 듣도록 하기
+        IntentFilter filter=new IntentFilter();
+        filter.addAction("download_progress");
+        DownloadReceiver receiver=new DownloadReceiver();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            registerReceiver(receiver,filter,Context.RECEIVER_NOT_EXPORTED);
+        }
+    }
+    
+    //progress 를 듣는 역활
+    class DownloadReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            //DownloadService에서 보낸 intent 에서 progress 꺼내기
+            int progress=intent.getIntExtra("progress",0);
+            downloadPb.setProgress(progress);
+
+        }
+    }
+
+}
